746e0985365b5cd6dedd4bb735a27038de4ac7de
ChelseaVK <cvanklaveren5@gmail.com>
2025-06-20 15:32:09 -0500

    Updated css and javascript; focused on user experience

---
+++ scr/script.js
@@ -0,0 +1,32 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "5b7e32442badt617fc2dae0afo330c92";
+  let prompt = `User instructions: Generate a recipe with ${instructionsInput.value}`;
+  let context =
+    "You are an expert with ingredients and food. Your mission is to generate a recipe and separate each line with a <br />. Include the ingredients and instructions on a separate line. Make sure to follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">⏳ Generating a recipe including ${instructionsInput.value}...</div>`;
+
+  console.log("Generating recipe...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
